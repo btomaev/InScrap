19a6ef23a505c34cdd195b6e0e14f8896b9b8bda
btomaev <65013732+btomaev@users.noreply.github.com>
2022-09-14 22:25:11 +0300

    Delete InScrap directory

--- InScrap/acconv.py
+++
@@ -1,8 +0,0 @@
-import json
-path = input('source>>> ')
-
-with open(path, 'r') as f:
-	data = [i.strip().split(':') for i in f.readlines()]
-	out = [{"login":i[0], "password":i[1]} for i in data]
-	print(json.dumps(out))
-input()
